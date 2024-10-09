cf658756adbc0af3b3d32d159091d39cf2b73395
potat0331 <mathcdy@naver.com>
2024-10-09 07:21:25 +0000

    착수소리 추가

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const canvas = document.querySelector(".canvas");
 const context = canvas.getContext("2d");
-
-const startButton = document.querySelector(".start");
+const resetButton = document.querySelector(".reset");
+const moveSound = new Audio("TAK.WAV");
 
 let area4game = new area4();
 area4game.drawboard(context);
 
-startButton.addEventListener("click", () => {
-  alert("시작");
+resetButton.addEventListener("click", () => {
+  area4game = new area4();
+  area4game.drawboard(context);
 });
 
 canvas.addEventListener("click", (e) => {
@@ -18,14 +19,17 @@ canvas.addEventListener("click", (e) => {
   } //보드판 밖 클릭 시 무시
 
   if (
+    // getAllAvailableMove 함수에서 지금 착수 한 곳의 index값을 찾을 수 없다면
     !area4game
       .getAllAvailableMove()
       .find((element) => element == inputX + 9 * inputY)
   ) {
+    //착수 할 수 없는 곳 이므로 클릭시 무시
     alert("착수 할 수 없는 곳 입니다.");
     return;
-  } //착수 할 수 없는 곳 클릭시 무시
+  }
 
   area4game.putStone(inputX, inputY); //수 정보 입력
+  moveSound.play();
   area4game.drawboard(context);
 });
